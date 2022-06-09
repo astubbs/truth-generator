6826c0cb251c1589ca5c00e2c1c730620ae3e006
Antony Stubbs <antony.stubbs@gmail.com>
2022-06-09 11:17:49 +0100

    Don't try to generate assertion methods for Legacy class' void return methods (#118)

--- generator/src/main/java/io/stubbs/truth/generator/internal/SubjectMethodGenerator.java
+++ generator/src/main/java/io/stubbs/truth/generator/internal/SubjectMethodGenerator.java
@@ -6,7 +6,6 @@ import io.stubbs.truth.generator.internal.model.ThreeSystem;
 import lombok.Getter;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.jboss.forge.roaster.model.source.MethodSource;
-import org.reflections.ReflectionUtils;
 
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
@@ -97,14 +96,16 @@ public class SubjectMethodGenerator extends AssertionMethodStrategy {
         // also get all other methods, regardless of their prefix
         Predicate<Method> exceptSetters = not(withPrefix("set"));
         Predicate<Method> exceptToers = not(withPrefix("to"));
-        var legacy = (legacyMode) ? getMethods(classUnderTest, exceptSetters, exceptToers) : Set.<Method>of();
+
+        var legacy = (legacyMode)
+                ? getMethods(classUnderTest, exceptSetters, exceptToers)
+                : Set.<Method>of();
 
         union.addAll(getters);
         union.addAll(issers);
         union.addAll(legacy);
 
         return removeForJdkTarget(system.getClassUnderTest(), removeOverridden(union));
-//    return removeOverridden(union);
     }
 
     private Collection<Method> removeForJdkTarget(Class<?> clazz, Collection<Method> methods) {
@@ -123,21 +124,25 @@ public class SubjectMethodGenerator extends AssertionMethodStrategy {
         }
     }
 
-    private Collection<Method> getMethods(Class<?> classUnderTest, Predicate<Method>... prefix) {
+    private Collection<Method> getMethods(Class<?> classUnderTest, Predicate<Method>... rawPredicates) {
         // if shaded, can't access package private methods
         boolean isShaded = context.isShaded();
-        Predicate<Method> skip = (ignore) -> true;
+        Predicate<Method> skip = ignore -> true;
         Predicate<Method> shadedPredicate = (isShaded) ? withModifier(PUBLIC) : skip;
 
-        List<Predicate<? super Method>> predicatesCollect = Arrays.stream(prefix).collect(Collectors.toList());
+        List<Predicate<? super Method>> predicatesCollect = Arrays.stream(rawPredicates).collect(Collectors.toList());
         predicatesCollect.add(shadedPredicate);
         predicatesCollect.add(not(withModifier(PRIVATE)));
         predicatesCollect.add(not(withModifier(PROTECTED)));
         predicatesCollect.add(withParametersCount(0));
 
+        // filter out anything that returns nothing (pure side effect methods)
+        Predicate<Method> exceptVoidReturn = (not(method -> method.getReturnType().equals(void.class)));
+        predicatesCollect.add(exceptVoidReturn);
+
         Predicate<? super Method>[] predicates = predicatesCollect.toArray(new Predicate[0]);
-        return removeForJdkTarget(classUnderTest, ReflectionUtils.getAllMethods(classUnderTest, predicates));
-//    return ReflectionUtils.getAllMethods(classUnderTest, predicates);
+        Set<Method> filteredMethods = getAllMethods(classUnderTest, predicates);
+        return removeForJdkTarget(classUnderTest, filteredMethods);
     }
 
     private Collection<Method> removeOverridden(final Collection<Method> getters) {

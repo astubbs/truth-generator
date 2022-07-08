61770a0f84c073791ceaadecd1047d7000d9a7a4
Antony Stubbs <antony.stubbs@gmail.com>
2022-07-08 14:41:01 +0100

    place holder test

---
+++ generator/src/test/java/io/stubbs/truth/generator/internal/AssertionEntryPointGeneratorTest.java
@@ -0,0 +1,17 @@
+package io.stubbs.truth.generator.internal;
+
+import org.junit.Test;
+
+/**
+ * @author Antony Stubbs
+ */
+public class AssertionEntryPointGeneratorTest {
+//todo
+
+    /**
+     * Test for colliding types - types of the same name but exist in different packages
+     */
+    @Test
+    public void sameTypeNameDifferentPackageCollision() {
+    }
+}
